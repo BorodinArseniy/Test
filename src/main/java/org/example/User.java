1cdcec9603de707444e2991e649f26d055b0e982
Arseniy <b9692861615@gmail.com>
2023-03-05 12:47:38 +0300

    1

---
+++ src/main/java/org/example/User.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class User {
+    String login;
+    String email;
+
+    
+
+
+}
---
+++ src/test/java/UserTest.java
@@ -0,0 +1,6 @@
+public class UserTest {
+    String login;
+    String password;
+
+    
+}
