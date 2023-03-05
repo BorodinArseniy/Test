9aa281ef0859f5875ca2846a6fe26b2899e98382
Arseniy <b9692861615@gmail.com>
2023-03-05 18:26:46 +0300

    1

--- src/main/java/org/example/User.java
+++ src/main/java/org/example/User.java
@@ -1,10 +1,30 @@
 package org.example;
 
 public class User {
-    String login;
-    String email;
+    private String login;
+    private String email;
 
-    
+    public User(String login, String email) {
+        this.login = login;
+        this.email = email;
+    }
 
+    public User() {
+    }
 
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
---
+++ src/test/java/org/example/UserTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    private static final String NULL_FIELD = null;
+    private static final String EMPTY_FIELD = "";
+
+    private static User user1 = new User();
+    private static User user2 = new User("RandomLogin", "RandomLogin.gmail.com");
+    private static User user3 = new User("RandomLogin.gmail.com", "RandomLogin.gmail.com");
+
+    @Test
+    public void areValuesEqualNulls() {
+        //Assertions.assertTrue(user1.getEmail() == NULL_FIELD && user1.getLogin() == NULL_FIELD);
+        Assertions.assertNull(user1);
+    }
+
+    @Test
+    public void areValuesInitialized(){
+        Assertions.assertTrue(!(user2.getEmail().isEmpty()&&user2.getEmail().isBlank()));
+        Assertions.assertTrue(!(user2.getLogin().isEmpty()&&user2.getLogin().isBlank()));
+    }
+
+    @Test
+    public void isEmailValueCorrectlyInitialized(){
+        Assertions.assertTrue(user2.getEmail().contains("@") && user2.getEmail().contains("."));
+    }
+
+    @Test
+    public void doEmailAndLoginEqual() {
+        Assertions.assertNotEquals(user3.getEmail(), user3.getLogin());
+    }
+
+
+
+}
